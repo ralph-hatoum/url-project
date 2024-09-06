f6849fb993aa197bebd5e62a1c8ed45ed64df9f7
ralph-hatoum <hatoumralph@gmail.com>
2024-09-06 15:37:59 +0200

    stats tables in DB

--- init.sql
+++ init.sql
@@ -4,3 +4,17 @@ CREATE TABLE url_table (
     id VARCHAR(255) PRIMARY KEY,
     url TEXT UNIQUE
 );
+
+CREATE TABLE performance (
+    request_type VARCHAR(255) PRIMARY KEY,
+    duration FLOAT
+);
+
+CREATE TABLE request_count(
+    request_type VARCHAR(255) PRIMARY KEY,
+    number_of_requests INTEGER
+);
+
+INSERT INTO request_count(request_type, number_of_requests) VALUES ('get_link_from_id', 0);
+
+INSERT INTO request_count(request_type, number_of_requests) VALUES ('new_short_link', 0);
